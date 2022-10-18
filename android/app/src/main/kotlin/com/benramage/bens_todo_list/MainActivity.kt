b80738d356d7e526f9c7429e35e8c485c55c888b
Benjamin Ramage <catchbenramage@gmail.com>
2022-10-19 09:20:03 +1000

    Initial commit

---
+++ android/app/src/main/kotlin/com/benramage/bens_todo_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.benramage.bens_todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:bens_todo_list/app.dart';
+
+void main() {
+  runApp(const App());
+}
+
+
+
